6c824166c2f8c34facf9ee69d72fba5a4f705670
vinay <vinay1289@gmail.com>
2021-05-06 15:39:09 +0530

    code for location

---
+++ src/main/java/hospital/App.java
@@ -0,0 +1,13 @@
+package hospital;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
